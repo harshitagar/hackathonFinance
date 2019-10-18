a46091624cba7dcddd0bb8011677b2164b7646c1
Simranjeet Singh <sjsingh1809@gmail.com>
2019-10-19 00:20:16 +0530

    Home page and share page UI

--- controllers/mobile/frmHomeController/frmHomeController.js
+++ controllers/mobile/frmHomeController/frmHomeController.js
@@ -1,5 +1,18 @@
 define({ 
 
  //Type your controller code here 
+  onNavigate:function(){
+    this.view.preShow=this.PreShow;
+    this.view.postShow=this.PostShow;
+  },
+  PreShow:function(){
+    this.view.flxTab1.onClick=()=>{animate(this.view.flxAnimate,{"left":"10%"});};
+    this.view.flxTab2.onClick=()=>{animate(this.view.flxAnimate,{"left":"40%"});};
+    this.view.flxTab3.onClick=()=>{animate(this.view.flxAnimate,{"left":"70%"});};
+    
+  },
+  PostShow:function(){
+    
+  },
 
  });
--- controllers/mobile/frmHomeController/frmHomeControllerActions.js
+++ controllers/mobile/frmHomeController/frmHomeControllerActions.js
@@ -1,7 +1,11 @@
-define({ 
-
- /* 
- This is an auto generated file and any modifications to it may result in corruption of the action sequence.
- */
-
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for flxShare **/
+    AS_FlexContainer_de0f039ce3854fa5a5d641c3f5ee9686: function AS_FlexContainer_de0f039ce3854fa5a5d641c3f5ee9686(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmShare");
+        ntf.navigate();
+    }
 });
