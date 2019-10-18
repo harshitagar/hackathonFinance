f547fda005aa0ad7d6a8bb014666edf865e04526
Simranjeet Singh <sjsingh1809@gmail.com>
2019-10-19 01:32:29 +0530

    Fixes

--- controllers/mobile/frmHomeController/frmHomeControllerActions.js
+++ controllers/mobile/frmHomeController/frmHomeControllerActions.js
@@ -3,7 +3,7 @@ define({
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
     /** onClick defined for flxShare **/
-    AS_FlexContainer_de0f039ce3854fa5a5d641c3f5ee9686: function AS_FlexContainer_de0f039ce3854fa5a5d641c3f5ee9686(eventobject) {
+    AS_FlexContainer_c5d4db2c4fd0491cb00e2fd3b4cc62b0: function AS_FlexContainer_c5d4db2c4fd0491cb00e2fd3b4cc62b0(eventobject) {
         var self = this;
         var ntf = new kony.mvc.Navigation("frmShare");
         ntf.navigate();
