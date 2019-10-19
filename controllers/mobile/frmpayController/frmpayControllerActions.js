3d165747bd6b07496d7b0191e27ffb90823ac014
Amritpal singh <amritpal.singh@kony.com>
2019-10-20 02:18:06 +0530

    frmPay form

---
+++ controllers/mobile/frmpayController/frmpayControllerActions.js
@@ -0,0 +1,17 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for flxBack **/
+    AS_FlexContainer_ea558bd36aa9462d8466d211eaffb8e7: function AS_FlexContainer_ea558bd36aa9462d8466d211eaffb8e7(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmHome");
+        ntf.navigate();
+    },
+    /** onClick defined for flxBackAddGrp **/
+    AS_FlexContainer_dd8c8580e344470f9c2c45e1ecd60528: function AS_FlexContainer_dd8c8580e344470f9c2c45e1ecd60528(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmHome");
+        ntf.navigate();
+    }
+});
