55e4e2fcbf4a088d40fcf9e4b74d747af46fefc3
Amritpal singh <amritpal.singh@kony.com>
2019-10-19 13:52:51 +0530

    controller actions

--- controllers/mobile/frmHomeController/frmHomeControllerActions.js
+++ controllers/mobile/frmHomeController/frmHomeControllerActions.js
@@ -2,15 +2,15 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
-    /** onClick defined for flxShare **/
-    AS_FlexContainer_ec14bd76c9eb47e5b8822cd42972a415: function AS_FlexContainer_ec14bd76c9eb47e5b8822cd42972a415(eventobject) {
-        var self = this;
-        return commonNavigateFunction.call(this, "frmShare");
-    },
     /** onClick defined for flxTab3 **/
     AS_FlexContainer_eb633951b2f34a22a4e160ff7ef7be80: function AS_FlexContainer_eb633951b2f34a22a4e160ff7ef7be80(eventobject) {
         var self = this;
         var ntf = new kony.mvc.Navigation("frmOffersNearMe");
         ntf.navigate();
+    },
+    /** onClick defined for flxShare **/
+    AS_FlexContainer_ec14bd76c9eb47e5b8822cd42972a415: function AS_FlexContainer_ec14bd76c9eb47e5b8822cd42972a415(eventobject) {
+        var self = this;
+        return commonNavigateFunction.call(this, "frmShare");
     }
 });
--- controllers/mobile/frmOffersNearMeController/frmOffersNearMeControllerActions.js
+++ controllers/mobile/frmOffersNearMeController/frmOffersNearMeControllerActions.js
@@ -2,4 +2,10 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
+    /** onClick defined for flxBack **/
+    AS_FlexContainer_f8f4c7d4589e417eabbdc060144c60d6: function AS_FlexContainer_f8f4c7d4589e417eabbdc060144c60d6(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmHome");
+        ntf.navigate();
+    }
 });
