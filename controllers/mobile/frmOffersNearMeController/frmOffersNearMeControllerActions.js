27b429f235436eac88c5f004809d1843485fa931
Simranjeet Singh <sjsingh1809@gmail.com>
2019-10-20 02:04:14 +0530

    maps changes

--- controllers/mobile/frmOffersNearMeController/frmOffersNearMeControllerActions.js
+++ controllers/mobile/frmOffersNearMeController/frmOffersNearMeControllerActions.js
@@ -7,11 +7,5 @@ define({
         var self = this;
         var ntf = new kony.mvc.Navigation("frmHome");
         ntf.navigate();
-    },
-    /** onClick defined for CopyflxBack0j0d9e35c276549 **/
-    AS_FlexContainer_a6073571b20649d5a41bf66f9cdb66c3: function AS_FlexContainer_a6073571b20649d5a41bf66f9cdb66c3(eventobject) {
-        var self = this;
-        var ntf = new kony.mvc.Navigation("frmHome");
-        ntf.navigate();
     }
 });
