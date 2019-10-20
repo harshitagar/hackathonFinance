3e0d8532dad179ea28f3b21b992f5827a31b887e
Gagan Lohia <gagandeep.lohia@kony.com>
2019-10-20 12:47:07 +0530

    Charity Added

---
+++ controllers/mobile/frmCharityController/frmCharityControllerActions.js
@@ -0,0 +1,17 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for flxBack **/
+    AS_FlexContainer_c87cce25a68840c28f887ee4aa5faabb: function AS_FlexContainer_c87cce25a68840c28f887ee4aa5faabb(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmHome");
+        ntf.navigate();
+    },
+    /** onClick defined for flxBackAddGrp **/
+    AS_FlexContainer_aa1b29621222488fb7b123c129114f3b: function AS_FlexContainer_aa1b29621222488fb7b123c129114f3b(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmHome");
+        ntf.navigate();
+    }
+});
